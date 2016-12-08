b1e304a6d605595626b6153fad8c570fd7f9af2b
nursike <nursultan.yerezhepov@gmail.com>
2016-12-08 17:51:08 +0600

    delete campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -78,6 +78,17 @@ router.put('/:id', function(req, res) {
    });
 });
 
+// Destroy campgrounds route
+router.delete("/:id", function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
